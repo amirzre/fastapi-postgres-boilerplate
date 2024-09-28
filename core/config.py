357e4013023524c32fd86d1f01caefc7b621f30e
Amir Zare <amirzare309@yahoo.com>
2024-09-28 04:57:20 -0400

    fix: add new config according to pydantic v2

--- core/config.py
+++ core/config.py
@@ -1,108 +1,43 @@
-from typing import Any, Dict, List, Optional, Union
+from enum import auto
+from secrets import token_urlsafe
 
-from pydantic import AnyHttpUrl, BaseSettings, EmailStr, PostgresDsn, validator
+from pydantic import PostgresDsn, RedisDsn
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+from core.enum import StrEnum
 
 
-class AsyncPostgresDsn(PostgresDsn):
-    allowed_schemes = {"postgres+asyncpg", "postgresql+asyncpg"}
+class EnvironmentType(StrEnum):
+    DEVELOPMENT = auto()
+    PRODUCTION = auto()
+    TEST = auto()
 
 
-class Settings(BaseSettings):
-    POSTGRES_SERVER: str
-    POSTGRES_USER: str
-    POSTGRES_PASSWORD: str
-    POSTGRES_DB: str
-    POSTGRES_TEST_DB: str
-    POSTGRES_PORT: str
+class BaseConfig(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env", env_prefix="", env_nested_delimiter="__", case_sensitive=True)
 
-    PROJECT_NAME: str
-    API_V1_STR: str = "/api/v1"
-    FIRST_SUPERUSER: EmailStr
-    FIRST_SUPERUSER_PASSWORD: str
+
+class Config(BaseConfig):
+    APP_TITLE: str = "FastAPI Application"
     DEBUG: bool = False
+    ENVIRONMENT: EnvironmentType = EnvironmentType.DEVELOPMENT
+    WORKERS: int = 1
 
-    USERS_OPEN_REGISTRATION: bool = True
-    SECRET_KEY: str
-    # 60 minutes * 24 hours * 1 day = 1 days
-    ACCESS_TOKEN_EXPIRE_MINUTES: int
-    REFRESH_TOKEN_EXPIRE_MINUTES: int
-    ALGORITHM: str = "HS256"
+    POSTGRES_URL: PostgresDsn = "postgresql+asyncpg://postgres:postgresql@127.0.0.1:5432/movie-reservation"
+    POSTGRES_TEST_URL: PostgresDsn
+    REDIS_URL: RedisDsn = "redis://localhost:6379/0"
 
-    RABBITMQ_USERNAME: str
-    RABBITMQ_PASSWORD: str
-    RABBITMQ_HOST: str
-    RABBITMQ_PORT: str
+    SECRET_KEY: str = token_urlsafe(32)
+    JWT_ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24
+    REFRESH_TOKEN_EXPIRE_MINUTES: int = 60 * 24
 
-    REDIS_SERVER: str
-    REDIS_PORT: int
-    REDIS_PASSWORD: str
-    REDIS_TIMEOUT: Optional[int] = 5
+    SQLALCHEMY_POOL_SIZE: int = 15
+    SQLALCHEMY_POOL_TIMEOUT: int = 30
+    SQLALCHEMY_POOL_RECYCLE: int = 3600
+    SQLALCHEMY_MAX_OVERFLOW: int = 5
 
-    BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = []
-    authjwt_secret_key: str = "secret"
-    @validator("BACKEND_CORS_ORIGINS", pre=True)
-    def assemble_cors_origins(cls, v: Union[str, List[str]]) -> Union[List[str], str]:
-        if isinstance(v, str) and not v.startswith("["):
-            return [i.strip() for i in v.split(",")]
-        elif isinstance(v, (list, str)):
-            return v
-        raise ValueError(v)
-
-    SQLALCHEMY_DATABASE_URI: Optional[PostgresDsn] = None
-
-    @validator("SQLALCHEMY_DATABASE_URI", pre=True)
-    def assemble_db_connection(cls, v: Optional[str], values: Dict[str, Any]) -> Any:
-        if isinstance(v, str):
-            return v
-        return PostgresDsn.build(
-            scheme="postgresql",
-            user=values.get("POSTGRES_USER"),
-            password=values.get("POSTGRES_PASSWORD"),
-            host=values.get("POSTGRES_SERVER"),
-            path=f"/{values.get('POSTGRES_DB') or ''}",
-            port=values.get('POSTGRES_PORT')
-        )
-    
-
-    
-    SQLALCHEMY_TEST_DATABASE_URI: Optional[PostgresDsn] = None
-
-    @validator("SQLALCHEMY_TEST_DATABASE_URI", pre=True)
-    def assemble_test_db_connection(
-        cls, v: Optional[str], values: Dict[str, Any]
-    ) -> Any:
-        if isinstance(v, str):
-            return v
-        return PostgresDsn.build(
-            scheme="postgresql",
-            user=values.get("POSTGRES_USER"),
-            password=values.get("POSTGRES_PASSWORD"),
-            host=values.get("POSTGRES_SERVER"),
-            path=f"/{values.get('POSTGRES_TEST_DB') or ''}",
-            port=values.get('POSTGRES_PORT')
-        )
+    RELEASE_VERSION: str = "1.0"
 
 
-
-    SQLALCHEMY_DATABASE_ASYNC_URI: Optional[AsyncPostgresDsn] = None
-
-    @validator("SQLALCHEMY_DATABASE_ASYNC_URI", pre=True)
-    def assemble_async_db_connection(
-        cls, v: Optional[str], values: Dict[str, Any]
-    ) -> Any:
-        if isinstance(v, str):
-            return v
-        return AsyncPostgresDsn.build(
-            scheme="postgresql+asyncpg",
-            user=values.get("POSTGRES_USER"),
-            password=values.get("POSTGRES_PASSWORD"),
-            host=values.get("POSTGRES_SERVER"),
-            port=values.get("POSTGRES_PORT"),
-            path=f"/{values.get('POSTGRES_DB') or ''}",
-        )
-
-    class Config:
-        env_file = ".env"
-
-
-settings = Settings()
+config: Config = Config()
