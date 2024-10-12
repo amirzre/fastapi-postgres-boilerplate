4ba50d8d916e3ca35cb4c264f3859e4a025d5a6f
Amir Zare <amirzare309@yahoo.com>
2024-10-12 04:16:21 -0400

    feat: add get user controller

--- app/controllers/user.py
+++ app/controllers/user.py
@@ -1,6 +1,9 @@
+from uuid import UUID
+
 from app.models import User
 from app.repositories import UserRepository
 from core.controller import BaseController
+from core.exceptions import NotFoundException
 
 
 class UserController(BaseController[User]):
@@ -12,4 +15,8 @@ class UserController(BaseController[User]):
         super().__init__(model=User, repository=user_repository)
         self.user_repository = user_repository
 
-    ...
+    async def get_user(self, *, user_uuid: UUID) -> User:
+        user = await self.user_repository.get_by_uuid(uuid=user_uuid)
+        if not user:
+            raise NotFoundException(message="User not found.")
+        return user
