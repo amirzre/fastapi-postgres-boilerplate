2bd735af27578d29cd8ddab1cf95bfae367d775f
Amir Zare <amirzare309@yahoo.com>
2024-10-13 04:57:18 -0400

    fix: validate password with password validator type

--- app/schemas/request/user.py
+++ app/schemas/request/user.py
@@ -1,9 +1,7 @@
-import re
-
-from pydantic import BaseModel, EmailStr, Field, field_validator
+from pydantic import BaseModel, EmailStr, Field
 
 from app.models.user import UserRole
-from core.exceptions import BadRequestException
+from core.utils import PasswordValidator
 
 
 class RegisterUserRequest(BaseModel):
@@ -12,13 +10,4 @@ class RegisterUserRequest(BaseModel):
     last_name: str | None = Field(max_length=50, description="Lastname")
     role: UserRole | None = Field(description="User Role")
     activated: bool = Field(description="activated")
-    password: str = Field(max_length=50, min_length=8, description="Password")
-
-    @field_validator("password", mode="after")
-    def validate_password(cls, value: str) -> str:
-        password_pattern = re.compile(r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$")
-        if not password_pattern.match(value):
-            raise BadRequestException(
-                message="Password must contain at least 8 characters, including one uppercase letter, one lowercase letter, one number, and one special character."
-            )
-        return value
+    password: PasswordValidator = Field(max_length=50, min_length=8, description="Password")
