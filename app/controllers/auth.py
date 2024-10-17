25619cf1d756794589305ff73cb5346673f89ff9
Amir Zare <amirzare309@yahoo.com>
2024-10-17 04:05:10 -0400

    feat: add auth base controller

---
+++ app/controllers/auth.py
@@ -0,0 +1,11 @@
+from app.models import User
+from app.repositories import UserRepository
+from core.controller import BaseController
+
+
+class AuthController(BaseController[User]):
+    def __init__(self, user_repository: UserRepository):
+        super().__init__(model=User, repository=user_repository)
+        self.user_repository = user_repository
+
+    ...
