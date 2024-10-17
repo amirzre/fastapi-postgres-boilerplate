7569eb2ebbdb28f621669b9228c0ca6a2a366f42
Amir Zare <amirzare309@yahoo.com>
2024-10-17 03:55:14 -0400

    feat: add authentication handler for decode user info from jwt tokens

--- core/fastapi/dependencies/__init__.py
+++ core/fastapi/dependencies/__init__.py
@@ -1,4 +1,16 @@
+from .authentication import AuthenticationHandler
+from .current_user import get_authenticated_user, get_current_user, get_current_user_with_refresh_token
 from .logging import Logging
-from .permission import AllowAll, IsAuthenticated, PermissionDependency
+from .permission import AllowAll, IsAdmin, IsAuthenticated, PermissionDependency
 
-__all__ = ["Logging", "AllowAll", "IsAuthenticated", "PermissionDependency"]
+__all__ = [
+    "Logging",
+    "AllowAll",
+    "IsAuthenticated",
+    "IsAdmin",
+    "PermissionDependency",
+    "AuthenticationHandler",
+    "get_authenticated_user",
+    "get_current_user",
+    "get_current_user_with_refresh_token",
+]
